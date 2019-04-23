1c0a5bec6e6c38e67d53a08ec2066b4ff941b2b1
ahmedmzaki <ahmedmzaki22@gmail.com>
2019-04-23 16:51:03 +0200

    still using ignore

---
+++ code.php
@@ -0,0 +1,33 @@
+<?php
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+echo 'User Real IP - '.getUserIpAddr();
+
+
+// Given password
+$password = 'user-input-pass';
+
+// Validate password strength
+$uppercase = preg_match('@[A-Z]@', $password);
+$lowercase = preg_match('@[a-z]@', $password);
+$number    = preg_match('@[0-9]@', $password);
+$specialChars = preg_match('@[^\w]@', $password);
+
+if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+    echo 'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
+}else{
+    echo 'Strong password.';
+}
+
+ ?>
