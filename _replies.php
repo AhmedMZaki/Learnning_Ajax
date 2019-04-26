5614b212dc3d62e0b46c1fa28e93be03919a8bdc
ahmedmzaki <ahmedmzaki22@gmail.com>
2019-04-26 18:02:21 +0200

    adding code

--- _replies.php
+++ _replies.php
@@ -1,1 +1,23 @@
+<?php
+$connection = mysqli_connect('localhost','root','root','social_auth');
+  if ($connection) {
+    if (isset($_GET['id'])) {
 
+        $id = (int)$_GET['id'];
+        $sql = "SELECT * FROM replays WHERE discussion_id=".$id;
+        $result = mysqli_query($connection,$sql);
+        $result_set = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+          $result_set[] = $row;
+        }
+         echo json_encode($result_set,JSON_FORCE_OBJECT);
+         mysqli_close($connection);
+         return;
+
+    }
+  } else {
+    return ;
+  }
+
+
+ ?>
